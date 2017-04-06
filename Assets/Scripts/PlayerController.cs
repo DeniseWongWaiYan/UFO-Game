a941590edcd45156aa92528fa7f4ae58af8c2bac
denise Wong <denisewongwaiyan@gmail.com>
2017-04-07 02:06:38 +0800

    Upload #1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Movement of player
+	void FixedUpdate () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveH, moveV);
+		rb2d.AddForce (movement * speed);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
